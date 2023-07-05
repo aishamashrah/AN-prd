8218c37e2827f0f48fd8d562208bd9eeb65314d8
aisha mashrah <aishamashrah@gmail.com>
2023-07-05 10:25:20 -0700

    commit

---
+++ my-app/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'pm-root',
+    template: `
+        <div>
+            <h1>{{ pageTitle }}</h1>
+       <pm-products></pm-products>
+        </div>
+    `
+})
+export class AppComponent {
+    title(title: any) {
+      throw new Error('Method not implemented.');
+    }
+    pageTitle: string = 'Acme Product Management';
+}
+
---
+++ my-app/src/app/prodects/Product.List.Component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './Product.List.Component.html'
+})
+export class ProductListComponent {
+  // Component logic here
+}
